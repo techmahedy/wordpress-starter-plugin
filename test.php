f8923c5fb65a4a65b2d3ccbe0c12ac8f930ff57b
techmahedy <mahedy150101@gmail.com>
2020-07-03 01:02:38 +0600

    delete test.php

--- test.php
+++
@@ -1,5 +0,0 @@
-  <h1 style="font-family:bold;"><?php echo esc_html('Garuda keen star rating plugin info', 'garuda-keen-rating'); ?></h1>
-    <h2><?php echo esc_html('Shortcode:', 'garuda-keen-rating') ?> <?php echo esc_attr('[keen_star_rating]', 'garuda-keen-rating'); ?></h2>
-    <p><?php echo esc_html('Use this shortcode to display this rating plugin. You can display any where. Just use this shortcode.', 'garuda-keen-rating'); ?></p>
-    <P><?php echo esc_html("Paste this code where you want to show : echo do_shortcode('[keen_star_rating]')", 'garuda-keen-rating'); ?> </P>
-    <form action="options.php" method="post" enctype="multipart/form-data">
